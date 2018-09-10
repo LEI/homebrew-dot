793e478795a7d438a0cd410c46d6a63ca8d1f2ea
goreleaserbot <goreleaser@carlosbecker.com>
2018-09-10 20:00:29 +0200

    Brew formula update for dot version v0.0.2

--- dot.rb
+++ dot.rb
@@ -3,7 +3,7 @@ class Dot < Formula
   homepage "https://github.com/LEI/dot"
   url "https://github.com/LEI/dot/releases/download/v0.0.2/dot-darwin-amd64.tar.gz"
   version "0.0.2"
-  sha256 "8d7e9e3f456924e6f1aa44b63642dddb18f809d2ffd1bd899c6bffeede39d8ed"
+  sha256 "075e4216ab6317dd5149eae58dcce79849faf2547350fa5c4dd2cd20a84be11d"
   
   depends_on "git"
 
